9fe467369728d623bd07fe4b34d711bf20a41e37
Daniel Popov <popovdaniil2000@gmail.com>
2019-11-23 21:59:05 +0300

    Created base of master

---
+++ base/base/polynom.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "polynom.h"
+
+
+polynom::polynom()
+{
+}
+
+
+polynom::~polynom()
+{
+}
---
+++ base/base/polynom.h
@@ -0,0 +1,8 @@
+#pragma once
+class polynom
+{
+public:
+	polynom();
+	~polynom();
+};
+
